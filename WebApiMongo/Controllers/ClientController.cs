5b5bc2598bb1ada375d38c6ce400b3b61d806232
artur <artur@DESKTOP-V0JU5PP>
2022-10-19 09:19:34 -0300

    Add project files.

---
+++ WebApiMongo/Controllers/ClientController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiMongo.Models;
+using WebApiMongo.Services;
+
+namespace WebApiMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ClientServices _clientService;
+
+        public ClientController(ClientServices clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Client>>Get() => _clientService.Get();
+        [HttpGet("{id:length(24)}", Name = "GetClient")]
+        public ActionResult<Client> Get(string id)
+        {
+            var client = _clientService.Get(id);
+
+            if (client == null)
+                return NotFound();
+
+            //return Ok(client);
+            return client;
+        }
+
+        [HttpPost]
+        public ActionResult<Client> Create(Client client)
+        {
+            _clientService.Create(client);
+            return CreatedAtRoute("GetClient", new { id = client.Id.ToString() }, client);
+        }
+    }
+}
---
+++ WebApiMongo/Utils/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiMongo.Utils
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ClientCollectionName { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string ConnectionString { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string DatabaseName { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+}
