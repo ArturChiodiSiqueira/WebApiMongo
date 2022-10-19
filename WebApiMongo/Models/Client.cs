c1af3b280b556af46f5872a5a86623fde93a1772
artur <artur@DESKTOP-V0JU5PP>
2022-10-19 09:50:42 -0300

    update service and add Bson

--- WebApiMongo/Models/Client.cs
+++ WebApiMongo/Models/Client.cs
@@ -1,7 +1,11 @@
-﻿namespace WebApiMongo.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApiMongo.Models
 {
     public class Client
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
     }
