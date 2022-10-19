2a3ec91516be4e7fcaeb404962244cf21b06514b
artur <artur@DESKTOP-V0JU5PP>
2022-10-19 13:59:27 -0300

    add description

--- WebApiMongo/Models/Address.cs
+++ WebApiMongo/Models/Address.cs
@@ -7,6 +7,7 @@ namespace WebApiMongo.Models
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; }
+        public string Description { get; set; }
         public string City { get; set; }
         public string Region { get; set; }
     }
