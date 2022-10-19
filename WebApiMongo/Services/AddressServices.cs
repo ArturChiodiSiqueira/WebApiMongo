d5420cd8c8be91c7b01b6e643272e7fa8774e2da
artur <artur@DESKTOP-V0JU5PP>
2022-10-19 13:49:54 -0300

    AddressService/Controller

--- WebApiMongo/Services/AddressServices.cs
+++ WebApiMongo/Services/AddressServices.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Driver;
+using System.Collections.Generic;
 using WebApiMongo.Models;
 using WebApiMongo.Utils;
 
@@ -15,5 +16,19 @@ namespace WebApiMongo.Services
             _address = database.GetCollection<Address>(settings.AddressCollectionName);
         }
 
+        public Address Create(Address address)
+        {
+            _address.InsertOne(address);
+            return address;
+        }
+
+        public List<Address> Get() => _address.Find<Address>(address => true).ToList(); //retorna EVERYBODY
+        public Address Get(string id) => _address.Find<Address>(address => address.Id == id).FirstOrDefault();//retorna quem achar.
+        public void Update(string id, Address addressIn)
+        {
+            _address.ReplaceOne(address => address.Id == id, addressIn);
+            Get(addressIn.Id);
+        }
+        public void Remove(Address clientIn) => _address.DeleteOne(client => client.Id == clientIn.Id);
     }
 }
